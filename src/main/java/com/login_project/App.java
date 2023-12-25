dd02654724ca3065f674e5e6be5b7f1f22d1aee0
Anandateertha <www.anandateertha@gmail.com>
2023-12-25 16:21:44 +0530

    first commit

---
+++ src/main/java/com/login_project/App.java
@@ -0,0 +1,10 @@
+package com.login_project;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("This is main ");
+    }
+}
---
+++ src/main/java/com/login_project/Login.java
@@ -0,0 +1,9 @@
+package com.login_project;
+
+public class Login {
+    
+    public String user()
+    {
+        return "Login project package created successfully!";
+    }
+}
